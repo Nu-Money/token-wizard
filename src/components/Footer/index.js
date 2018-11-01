d969b50c75ef4d6b0fe9d9ecb64277ef53271c91
tobiyahtafari <44656588+tobiyahtafari@users.noreply.github.com>
2018-11-01 12:02:30 -0400

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -13,9 +13,9 @@ export const Footer = () => {
         <p className="rights">{moment().format('YYYY')} Hosted By: POA Network. All rights reserved.</p>
         <Link className="logo" to='/'/>
         <div className="socials">
-          <a href="https://twitter.com/poanetwork" className="social social_twitter">Twitter</a>
-          <a href="https://poa.network" className="social social_oracles">POA Network</a>
-          <a href="https://t.me/oraclesnetwork" className="social social_telegram">Telegram</a>
+          <a href="https://twitter.com/thenumoney" className="social social_twitter">Twitter</a>
+          <a href="https://#" className="social social_oracles">NuMoney</a>
+          <a href="https://t.me/thenumoney" className="social social_telegram">Telegram</a>
           <a href="https://github.com/bertux/token-wizard" className="social social_github">GitHub</a>
         </div>
       </div>
